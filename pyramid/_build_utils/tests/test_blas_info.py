ec20cd4a4fdb65334794315b499237046d470b85
Taylor Smith <tgsmith61591@gmail.com>
2017-06-02 11:26:36 -0500

    Ignore blas info on coverage.

--- pyramid/_build_utils/tests/test_blas_info.py
+++
@@ -1,8 +0,0 @@
-# this is really just here for the coverage. The only
-# time this function is ever called is when we're running setup
-
-from pyramid._build_utils import get_blas_info
-
-
-def test_blas():
-    _ = get_blas_info()
