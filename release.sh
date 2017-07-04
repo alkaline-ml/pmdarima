34367625f01f8d1ce20260f4b88464f19e4bec9f
Taylor Smith <tgsmith61591@gmail.com>
2017-07-04 13:30:21 -0500

    Update release script

--- release.sh
+++ release.sh
@@ -12,10 +12,16 @@ OWNER=tgsmith61591
 # current Git branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
+# function to bail. Abstract into single function (rather than multiple exit calls)
+# so can pass for testing and make sure the whole script functions...
+bail() {
+    exit 12
+}
+
 # if branch is NOT master, error out
 if [[ "$branch" != "master" ]]; then
     echo "Not on master! Branch=$branch. Auto-tagging/release should only occur on master"
-    exit -6
+    bail
 fi
 
 # Get the version of pyramid - this is a major hack
@@ -28,13 +34,14 @@ versionLabel=v$VERSION_NUMBER
 # If there is a 'dev' anything suffix in the versionLabel, fail out. Need to increment version
 if [[ $versionLabel == *"dev"* ]]; then
     echo "Version number contains 'dev' suffix. Re-version then run again."
-    exit -3
+    bail
 elif git diff-index --quiet HEAD --; then
     # no changes - master has been pushed
     echo "Tagging/release version $versionLabel"
 else
     # changes need to be pushed
     echo "Local changes in git repo need pushing before tagging release."
+    bail
 fi
 
 # The release in pypi will reference an archive link. Need to ensure this link
@@ -42,4 +49,5 @@ fi
 # the JSON string here.. this will also create the tag on Git!
 ACCESS_TOKEN=`cat ACCESS_TOKEN`
 API_JSON=$(printf '{"tag_name": "%s", "target_commitish": "master", "name": "%s", "body": "Release of %s", "draft": false, "prerelease": false}' $versionLabel $versionLabel $versionLabel)
+echo "Curling JSON: ${API_JSON}"
 curl --data "$API_JSON" https://api.github.com/repos/${OWNER}/${REPO}/releases?access_token=${ACCESS_TOKEN}
