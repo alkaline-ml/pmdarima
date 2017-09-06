02f645b50e54b82937808790054a04482f713b5a
tgsmith61591 <tgsmith61591@gmail.com>
2017-09-06 18:44:37 -0500

    [ci skip] test publish script

--- doc/publish_gh_pages.sh
+++ doc/publish_gh_pages.sh
@@ -16,32 +16,6 @@ set -e
 # You will want to run this script from
 # its current location (i.e. sh publish_gh_pages.sh)
 
-#########
-# STEPS #
-#########
-
-# This script will check if the number of input arguments are 0
-#   A) If arguments == 0
-
-#       1) cd doc && make clean html         # purge old docs and create new
-#       2) cp -R ../ghpages/html ../         # copy html folder to base directory of master
-#       3) git stash                         # delete unwanted/uncommited changes to master
-#       4) git checkout gh-pages             # switch to gh-pages
-#       5) git pull origin gh-pages          # pull in latest changes from gh-pages
-#       6) cp -R ../html/* ..                # copy new html to base directory of gh-pages
-#       7) rm -r ../html                     # remove html folder
-#       8) git add -A                        # add the new and modified files
-#       9) git commit -m "$COMMIT_MESSAGE"   # commit the added changes
-#       10) git push origin gh-pages         # push the changes to gh-pages
-#       11) git checkout master              # return to master
-#
-#   B) If arguments != 0
-#       1) print messages on proper usage
-
-#############
-# EXECUTION #
-#############
-
 if test $# -eq 0; then
 
     VERSION_NUMBER=`python -c 'import pyramid as p; '\
@@ -49,39 +23,44 @@ if test $# -eq 0; then
                    'print(str(pv(p.__version__)))'`
     COMMIT_MESSAGE="Update for version - $VERSION_NUMBER"
     COMMIT_TIME=`date "+%m/%d/%Y %H:%M"`
+    CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    echo "Current branch: ${CURRENT_BRANCH}"
 
-    echo "\n\nSee note A.1) make clean html\n\n"
-    make clean html
-
-    echo "\n\nSee note A.2) ../gh-pages/html ../\n\n"
-    cp -R ../gh-pages/html ../
-
-    echo "\n\nSee note A.3) git stash\n\n"
+    # checkout gh-pages
+    echo "\n\nSee note A.1) git stash\n\n"
+    cd ..
     git stash
 
-    echo "\n\nSee note A.4) git checkout gh-pages\n\n"
+    echo "\n\nSee note A.2) git checkout gh-pages\n\n"
     git fetch
     git checkout gh-pages
 
-    echo "\n\nSee note A.5) git pull origin gh-pages\n\n"
+    echo "\n\nSee note A.3) git pull origin gh-pages\n\n"
     git pull origin gh-pages
 
-    echo "\n\nSee note A.6)cp -R ../html/* ..\n\n"
-    cp -R ../html/* ..
+    echo "\n\nSee note A.4) git merge --no-ff ${CURRENT_BRANCH}\n\n"
+    git merge --no-ff ${CURRENT_BRANCH}
 
-    echo "\n\nSee note A.7)rm -r ../html"
-    rm -r ../html
+    echo "\n\nSee note A.5) make clean html\n\n"
+    cd doc && make clean html
 
-    echo "\n\nSee note A.6) git add -A\n\n"
-    git add -A
+    echo "\n\nSee note A.6) on removing everything"
+    # delete everything pretty much
+    rm -rf ../.gitignore ../*.yml
 
-    echo "\n\nSee note A.7) git commit -m '$COMMIT_MESSAGE'\n\n"
-    git commit -m "$COMMIT_MESSAGE"
+    echo "\n\nSee note A.7) cp _build/html/* ../\n\n"
+    cp -R _build/html/* ../
 
-    echo "\n\nSee note A.8) git push origin gh-pages\n\n"
+    echo "\n\nSee note A.8) git add -A\n\n"
+    cd .. && git add -A
+
+    echo "\n\nSee note A.9) git commit -m '$COMMIT_MESSAGE'\n\n"
+    git commit -m "[ci skip] $COMMIT_MESSAGE"
+
+    echo "\n\nSee note A.10) git push origin gh-pages\n\n"
     git push origin gh-pages
 
-    echo "\n\nSee note A.9) git checkout master\n\n"
+    echo "\n\nSee note A.11) git checkout ${CURRENT_BRANCH}\n\n"
     git checkout master
 
 else
