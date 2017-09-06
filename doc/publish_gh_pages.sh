ba7b5f04a62dc4a9cdf5e104304b4adaab60664d
tgsmith61591 <tgsmith61591@gmail.com>
2017-09-06 18:51:29 -0500

    [ci skip] Make sure we allow unrelated histories in publish merge

--- doc/publish_gh_pages.sh
+++ doc/publish_gh_pages.sh
@@ -39,7 +39,7 @@ if test $# -eq 0; then
     git pull origin gh-pages
 
     echo "\n\nSee note A.4) git merge --no-ff ${CURRENT_BRANCH}\n\n"
-    git merge --no-ff ${CURRENT_BRANCH}
+    git merge --no-ff --allow-unrelated-histories ${CURRENT_BRANCH}
 
     echo "\n\nSee note A.5) make clean html\n\n"
     cd doc && make clean html
