65b74e8b5bd33ffecb800f23c95402da19e94a1b
Taylor Smith <tgsmith61591@gmail.com>
2018-01-24 11:04:16 -0600

    Update ruby for OSX build

--- build_tools/travis/before_install.sh
+++ build_tools/travis/before_install.sh
@@ -7,6 +7,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   echo "Updating apt-get for Linux build"
   sudo apt-get -qq update
 # Because screw you, Travis:
-# elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-#   rvm get stable
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  echo "Updating Ruby for Mac OS build"
+  command curl -sSL https://rvm.io/mpapis.asc | gpg --import -;
+  rvm get stable
 fi
