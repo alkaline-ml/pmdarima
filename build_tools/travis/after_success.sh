b2092d9ae5b931974ca6915633684162e8799d07
Taylor Smith <tgsmith61591@gmail.com>
2018-02-12 07:21:01 -0600

    Not sure if rsync is preventing .coverage from making it into the coveralls dir

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -10,27 +10,23 @@ set +e  # because TRAVIS SUCKS
 
 # push coverage if necessary
 if [[ "$COVERAGE" == "true" ]]; then
-    echo "COVERAGE=true; moving .coverage to build dir"
 
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
-    rsync $TEST_DIR/.coverage $TRAVIS_BUILD_DIR --ignore-existing
+    cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
     cd $TRAVIS_BUILD_DIR
 
     # Ignore coveralls failures as the coveralls server is not
     # very reliable but we don't want travis to report a failure
     # in the github UI just because the coverage report failed to
     # be published.
-    echo "Running coverage"
     coveralls || echo "Coveralls upload failed"
 fi
 
 # make sure we have twine in case we deploy
-echo "Installing twine"
 pip install twine || "pip installing twine failed"
 
 # remove the .egg-info dir so Mac won't bomb on bdist_wheel cmd (absolute path in SOURCES.txt)
-echo "Removing egg info (if exists)"
 rm -r pyramid_arima.egg-info/ || echo "No local .egg cache to remove"
 
 # make a dist folder if not there, then make sure permissions are sufficient
