6a45bb68c6531f4f0123bca61c5f472e809072ea
Taylor Smith <tgsmith61591@gmail.com>
2018-09-21 08:58:33 -0500

    CI: Replace nose calls with pytest

--- pyramid/__check_build/tests/test_check_build.py
+++ pyramid/__check_build/tests/test_check_build.py
@@ -4,11 +4,14 @@ from __future__ import absolute_import
 
 from pyramid.__check_build import raise_build_error
 
-from nose.tools import assert_raises
+import pytest
 
 
 def test_raise_build_error():
     try:
+        # Raise a value error to pass into the raise_build_error
+        # to assert it turns it into an ImportError
         raise ValueError("this is a dummy err msg")
     except ValueError as v:
-        assert_raises(ImportError, raise_build_error, v)
+        with pytest.raises(ImportError):
+            raise_build_error(v)
