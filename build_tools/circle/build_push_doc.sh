8d602bef04ece682f642a7b69889f7a128fa8daa
tgsmith61591 <tgsmith61591@gmail.com>
2018-12-16 14:05:35 -0600

    CI: Always build versioned doc, but maybe don't deploy

--- build_tools/circle/build_push_doc.sh
+++ build_tools/circle/build_push_doc.sh
@@ -48,101 +48,99 @@ function deploy() {
 
 # If we're on master or develop, we'll end up deploying
 echo "Branch name: ${CIRCLE_BRANCH}"
-if [[ ${CIRCLE_BRANCH} == "master" || ${CIRCLE_BRANCH} == "develop" ]]; then
 
-  # Show the present files:
-  ls -la
+# Show the present files:
+ls -la
 
-  # On both of these, we'll need to remove the artifacts from the package
-  # build itself
-  declare -a leftover=("benchmarks"
-                       "build"
-                       "dist"
-                       "doc"
-                       "pmdarima"
-                       "pmdarima.egg-info")
+# On both of these, we'll need to remove the artifacts from the package
+# build itself
+declare -a leftover=("benchmarks"
+                     "build"
+                     "dist"
+                     "doc"
+                     "pmdarima"
+                     "pmdarima.egg-info")
 
-  # check for each left over file/dir and remove it
-  for left in "${leftover[@]}"
+# check for each left over file/dir and remove it
+for left in "${leftover[@]}"
+do
+  echo "Removing ${left}"
+  rm -rf ${left}
+done
+
+# If it's develop, we can simply rename the "html" directory as the
+# "develop" directory
+if [[ ${CIRCLE_BRANCH} == "develop" ]]; then
+
+  # Remove the existing 'dev' folder (if it's there. might not be the
+  # first time we do this)
+  if [[ -d "develop" ]]; then
+    rm -rf develop/
+  fi
+
+  # Rename the html dir
+  mv html develop
+  # That's it for dev
+
+# Otherwise it's master or another branch, which is a bit more involved.
+# Remove the artifacts from the previous deployment, move the new ones into the
+# folder as well as into the versioned folder. This won't be deployed unless
+# it's master
+else
+
+  # These are the web artifacts we want to remove from the base
+  declare -a artifacts=("_downloads"
+                        "_images"
+                        "_modules"
+                        "_sources"
+                        "_static"
+                        "auto_examples"
+                        "includes"
+                        "modules")
+
+  for artifact in "${artifacts[@]}"
   do
-    echo "Removing ${left}"
-    rm -rf ${left}
+    echo "Removing ${artifact}"
+    rm -rf ${artifact}
   done
 
-  # If it's develop, we can simply rename the "html" directory as the
-  # "develop" directory
-  if [[ ${CIRCLE_BRANCH} == "develop" ]]; then
+  # Make a copy of the html directory. We'll rename this as the versioned dir
+  echo "Copying html directory"
+  cp -a html html_copy
 
-    # Remove the existing 'dev' folder (if it's there. might not be the
-    # first time we do this)
-    if [[ -d "develop" ]]; then
-      rm -r develop/
-    fi
+  # Move the HTML contents into the local dir
+  mv html/* ./
+  rm -r html/
 
-    # Rename the html dir
-    mv html develop
-    # That's it for dev
+  # Get the new version. This overwrites the old one.
+  python -c "import pmdarima; print(pmdarima.__version__)" > VERSION
 
-  # Otherwise it's master, which is a bit more involved. Remove the artifacts
-  # from the previous deployment, move the new ones into the folder as well
-  # as into the versioned folder
-  else
-
-    # These are the web artifacts we want to remove from the base
-    declare -a artifacts=("_downloads"
-                          "_images"
-                          "_modules"
-                          "_sources"
-                          "_static"
-                          "auto_examples"
-                          "includes"
-                          "modules")
-
-    for artifact in "${artifacts[@]}"
-    do
-      echo "Removing ${artifact}"
-      rm -rf ${artifact}
-    done
-
-    # Make a copy of the html directory. We'll rename this as the versioned dir
-    echo "Copying html directory"
-    cp -a html html_copy
-
-    # Move the HTML contents into the local dir
-    mv html/* ./
-    rm -r html/
-
-    # Get the new version. This overwrites the old one.
-    python -c "import pmdarima; print(pmdarima.__version__)" > VERSION
-
-    # If the version already has a folder, we have to fail out. We don't
-    # want to overwrite an existing version's documentation
-    version=`cat VERSION`
-    echo "New version: ${version}"
-    if [[ -d ${version} ]]; then
-      echo "Version ${version} already exists!! Will not overwrite. Failing job."
-      exit 9
-    fi
-
-    # If we get here, we can simply rename the html_copy dir as the versioned
-    # directory to be deployed.
-    mv html_copy ${version}
+  # If the version already has a folder, we have to fail out. We don't
+  # want to overwrite an existing version's documentation
+  version=`cat VERSION`
+  echo "New version: ${version}"
+  if [[ -d ${version} ]]; then
+    echo "Version ${version} already exists!! Will not overwrite. Failing job."
+    exit 9
   fi
 
-  # we need this empty file for git not to try to build a jekyll project
-  touch .nojekyll
-  echo "Final directory contents:"
-  ls -la
+  # If we get here, we can simply rename the html_copy dir as the versioned
+  # directory to be deployed.
+  mv html_copy ${version}
+fi
 
-  # Finally, deploy the branch, but if it's a pull request, don't do anything
-  if [[ ! -z $CI_PULL_REQUEST ]]; then
-    echo "Will not deploy doc on pull request ($CI_PULL_REQUEST)"
-  else
-    # TODO: UNCOMMENT THIS AFTER WE VERIFY PR BRANCH NAMES!!!!
-    echo "Deploying documentation"
-    # deploy
-  fi
+# we need this empty file for git not to try to build a jekyll project
+touch .nojekyll
+echo "Final directory contents:"
+ls -la
 
+# Finally, deploy the branch, but if it's a pull request, don't do anything
+if [[ ! -z $CI_PULL_REQUEST ]]; then
+  echo "Will not deploy doc on pull request ($CI_PULL_REQUEST)"
+elif [[ ${CIRCLE_BRANCH} == "master" || ${CIRCLE_BRANCH} == "develop" ]]; then
+  # TODO: UNCOMMENT THIS AFTER WE VERIFY PR BRANCH NAMES!!!!
+  echo "Deploying documentation"
+  # deploy
 else
   echo "Not on master or develop. Will not deploy doc"
 fi
