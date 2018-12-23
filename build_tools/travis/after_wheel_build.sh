4c7525a807280ebd716f1a2daff8788fef38207b
Aaron Smith <asmith@talroo.com>
2018-12-23 10:01:01 -0600

    Added check for build artifacts after Travis build

---
+++ build_tools/travis/after_wheel_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Don't think we actually need this, because we always build, but it doesn't hurt
+if [[ "${DEPLOY}" != true ]]; then
+    # Not a release
+    exit 0
+fi
+
+# Check for presence of wheel and tarball post-build
+if [[ ! -f dist/*.whl ]] && [[ ! -f dist/*.tar.gz ]]; then
+    echo "Artifacts did not build successfully"
+    exit 1
+else
+    echo "Build artifacts created successfully"
+fi
