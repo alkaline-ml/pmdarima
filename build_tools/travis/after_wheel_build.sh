fd3d4d47af80c60219259ef1ef5d8ece124c2383
Aaron Smith <aaronreidsmith@gmail.com>
2019-03-29 10:15:16 -0500

    Made message the same as appveyor

--- build_tools/travis/after_wheel_build.sh
+++ build_tools/travis/after_wheel_build.sh
@@ -14,4 +14,6 @@ fi
 if python -c "from twine.commands.check import check; check(['dist/*'])" | grep  "warning"; then
     echo "README will not render properly on PyPI"
     exit 1
+else
+    echo "README rendered appropriately"
 fi
