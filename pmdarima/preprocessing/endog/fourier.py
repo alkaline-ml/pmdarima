cff0dac8a511fbfad8751da4bb8e80a91efb0892
Aaron Smith <aaronreidsmith@gmail.com>
2019-03-31 18:56:31 -0500

    Start work on Fourier Transformation

--- pmdarima/preprocessing/endog/fourier.py
+++ pmdarima/preprocessing/endog/fourier.py
@@ -2,6 +2,9 @@
 #
 # Fourier transformations on endogenous arrays
 
+from scipy.fftpack import fft, ifft
+from sklearn.utils.validation import check_is_fitted
+
 from .base import BaseEndogTransformer
 
 __all__ = ['FourierEndogTransformer']
@@ -14,8 +17,10 @@ class FourierEndogTransformer(BaseEndogTransformer):
     ----------
     TODO
     """
-    def __init__(self):
-        pass
+    def __init__(self, n=None, axis=-1):
+
+        self.n = n
+        self.axis = axis
 
     def fit(self, y, exog=None):
         """Fit the transformer
@@ -56,7 +61,8 @@ class FourierEndogTransformer(BaseEndogTransformer):
         exog : array-like or None
             The exog array
         """
-        # TODO:
+        y, exog = self._check_y_exog(y, exog)
+        return fft(y, self.n, self.axis), exog
 
     def inverse_transform(self, y, exog=None):
         """Inverse transform a transformed array
@@ -81,4 +87,5 @@ class FourierEndogTransformer(BaseEndogTransformer):
         exog : array-like or None
             The inverse-transformed exogenous array
         """
-        # TODO:
+        y, exog = self._check_y_exog(y, exog)
+        return ifft(y, self.n, self.axis), exog
