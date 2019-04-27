f1e60283b44a1cede06ba31fe1932e5032097e85
tgsmith61591 <tgsmith61591@gmail.com>
2019-04-27 15:15:43 +0000

    [ci skip] Publishing updated documentation for 'master' branch

---
+++ _downloads/example_pipeline.py
@@ -0,0 +1,86 @@
+"""
+=========================
+Pipelines with auto_arima
+=========================
+
+
+Like scikit-learn, ``pmdarima`` can fit "pipeline" models. That is, a pipeline
+constitutes a list of arbitrary length comprised of any number of
+``BaseTransformer`` objects strung together ordinally, and finished with an
+``AutoARIMA`` object.
+
+The benefit of a pipeline is the ability to condense a complex sequence of
+stateful transformations into a single object that can call ``fit``,
+``predict`` and ``update``. It can also be serialized into *one* pickle file,
+which greatly simplifies your life.
+
+.. raw:: html
+
+   <br/>
+"""
+print(__doc__)
+
+# Author: Taylor Smith <taylor.smith@alkaline-ml.com>
+
+import numpy as np
+import pmdarima as pm
+from pmdarima import pipeline, preprocessing as ppc, arima
+from matplotlib import pyplot as plt
+
+# Load the data and split it into separate pieces
+data = pm.datasets.load_wineind()
+train, test = data[:150], data[150:]
+
+# Let's create a pipeline with multiple stages... the Wineind dataset is
+# seasonal, so we'll include a FourierFeaturizer so we can fit it without
+# seasonality
+pipe = pipeline.Pipeline([
+    ("fourier", ppc.FourierFeaturizer(m=12)),
+    ("arima", arima.AutoARIMA(stepwise=True, trace=1, error_action="ignore",
+                              seasonal=False,  # because we use Fourier
+                              transparams=False,
+                              suppress_warnings=True))
+])
+
+pipe.fit(train)
+print("Model fit:")
+print(pipe)
+
+# We can compute predictions the same way we would on a normal ARIMA object:
+preds, conf_int = pipe.predict(n_periods=10, return_conf_int=True)
+print("\nForecasts:")
+print(preds)
+
+# Let's take a look at the actual vs. the predicted values:
+fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+
+n_train = train.shape[0]
+x = np.arange(n_train + preds.shape[0])
+axes[0].plot(x[:n_train], train, alpha=0.75)
+# axes[0].scatter(x[n_train:], preds, alpha=0.4, marker='o')
+axes[0].scatter(x[n_train:], test[:preds.shape[0]], alpha=0.4, marker='x')
+axes[0].fill_between(x[n_train:], conf_int[:, 0], conf_int[:, 1],
+                     alpha=0.1, color='b')
+axes[0].set_title('Actual test samples vs. forecasts')
+axes[0].set_xlim((0, data.shape[0]))
+
+# We can also call `update` directly on the pipeline object, which will update
+# the intermittent transformers, where necessary:
+newly_observed, still_test = test[:15], test[15:]
+pipe.update(newly_observed, maxiter=10)
+
+# Calling predict will now predict from newly observed values
+new_preds = pipe.predict(still_test.shape[0])
+print(new_preds)
+
+x2 = np.arange(data.shape[0])
+n_trained_on = n_train + newly_observed.shape[0]
+
+axes[1].plot(x2[:n_train], train, alpha=0.75)
+axes[1].plot(x2[n_train: n_trained_on], newly_observed, alpha=0.75, c='orange')
+# axes[1].scatter(x2[n_trained_on:], new_preds, alpha=0.4, marker='o')
+axes[1].scatter(x2[n_trained_on:], still_test, alpha=0.4, marker='x')
+axes[1].set_title('Actual test samples vs. forecasts')
+axes[1].set_xlim((0, data.shape[0]))
+
+plt.show()
