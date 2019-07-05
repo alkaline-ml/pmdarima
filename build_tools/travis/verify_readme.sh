466b1882cae095296642438110a1e9b449d9c67a
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-05 11:20:32 -0500

    Remove travis build_tools

--- build_tools/travis/verify_readme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if python -c "from twine.commands.check import check; check(['dist/*'])" | grep  "warning"; then
-    echo "README will not render properly on PyPI"
-    exit 1
-else
-    echo "README rendered appropriately"
-fi
