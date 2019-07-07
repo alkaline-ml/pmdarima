68f0d0895ea9a433afd5b963b64fc7c09f6eca23
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-07 13:43:19 -0500

    Update version in __init__

--- pmdarima/__init__.py
+++ pmdarima/__init__.py
@@ -6,7 +6,23 @@
 
 import os as _os
 
-__version__ = "1.3.0-dev0"
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = "1.3.dev0"
 
 try:
     # this var is injected in the setup build to enable
