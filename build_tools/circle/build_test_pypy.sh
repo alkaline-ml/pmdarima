4fb6e91ef71e4554d291939fb8f45bd0113e0294
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-16 12:26:03 -0500

    Make pypy work on new base image

--- build_tools/circle/build_test_pypy.sh
+++ build_tools/circle/build_test_pypy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# pypy container default python is python2.7, so need to source the virtualenv
-# that is contained inside of the container
-source /pypy-env/bin/activate
+# Should be 3.6.xxx. If 2.7, PyPy build was screwed up
+python --version
 
 export LOKY_MAX_CPU_COUNT="2"  # for joblib parallelization
-pip install -vv -e .
+python -m pip install -vv -e .
 
 # Pytest is known to consume lots of memory for a large number of tests,
 # and Circle 2.0 limits 4GB per container.
