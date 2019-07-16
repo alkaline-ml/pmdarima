dc39f35e83a9482989f1eb1c805394301667afbd
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-16 10:56:51 -0500

    Update pypy circle test

--- build_tools/circle/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get -yq update
-apt-get -yq install libatlas-dev libatlas-base-dev liblapack-dev gfortran ccache
--- build_tools/circle/build_test_pypy.sh
+++ build_tools/circle/build_test_pypy.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
+# pypy container default python is python2.7, so need to source the virtualenv
+# that is contained inside of the container
+source /pypy-env/bin/activate
+
+export LOKY_MAX_CPU_COUNT="2"  # for joblib parallelization
 pip install -vv -e .
 
 # Pytest is known to consume lots of memory for a large number of tests,
