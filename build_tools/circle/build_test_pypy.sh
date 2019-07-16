e44360813c6ae6ba4ef8f164157804db2a59a857
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-16 07:51:32 -0500

    Use custom pypy base image

--- build_tools/circle/build_test_pypy.sh
+++ build_tools/circle/build_test_pypy.sh
@@ -1,35 +1,4 @@
 #!/usr/bin/env bash
-set -x
-set -e
-
-# Don't test with Conda here, use virtualenv instead
-pip install virtualenv
-
-if command -v pypy3; then
-    virtualenv -p $(command -v pypy3) pypy-env
-elif command -v pypy; then
-    virtualenv -p $(command -v pypy) pypy-env
-fi
-
-source pypy-env/bin/activate
-
-python --version
-which python
-
-extra_index_url="https://antocuni.github.io/pypy-wheels/ubuntu"
-
-pip install --extra-index ${extra_index_url} numpy Cython pytest
-pip install --extra-index ${extra_index_url} scipy
-pip install --extra-index ${extra_index_url} scikit-learn
-
-# Pandas has starting throwing issues in Pypy now...
-pip install "pandas==0.23.*" statsmodels matplotlib
-pip install --extra-index ${extra_index_url} pytest-mpl pytest-benchmark
-
-ccache -M 512M
-export CCACHE_COMPRESS=1
-export PATH=/usr/lib/ccache:$PATH
-export LOKY_MAX_CPU_COUNT="2"
 
 pip install -vv -e .
 
