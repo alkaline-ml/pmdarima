3ac2b75808cd8ede100650a442e075429aa2183e
Aaron Smith <aaronreidsmith@gmail.com>
2019-07-30 16:43:17 -0500

    Add output folder to conda build

--- build_tools/azure/conda_scripts/conda_convert.sh
+++ build_tools/azure/conda_scripts/conda_convert.sh
@@ -4,10 +4,10 @@
 
 # convert package to other platforms
 #platforms=( osx-64 linux-32 linux-64 win-32 win-64 )
-find $HOME/conda-bld/linux-64/ -name *.tar.bz2 | while read file
+find conda-bld/linux-64/ -name *.tar.bz2 | while read file
 do
     echo $file
-    conda convert --platform all $file  -o $HOME/conda-bld/
+    conda convert --platform all $file -o conda-bld/
 #    for platform in "${platforms[@]}"
 #    do
 #       conda convert --platform $platform $file  -o $HOME/conda-bld/
