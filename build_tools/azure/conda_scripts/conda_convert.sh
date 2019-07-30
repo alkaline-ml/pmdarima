14dee0bd50f3676ae61832bd04b09fd629ee9fbf
Aaron Smith <aaronreidsmith@gmail.com>
2019-07-30 15:51:15 -0500

    Added attribution

--- build_tools/azure/conda_scripts/conda_convert.sh
+++ build_tools/azure/conda_scripts/conda_convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Adapted from this gist: https://gist.github.com/giswqs/4eb62fb08658c8a200c4e18bb5e6270c
+
 # convert package to other platforms
 #platforms=( osx-64 linux-32 linux-64 win-32 win-64 )
 find $HOME/conda-bld/linux-64/ -name *.tar.bz2 | while read file
