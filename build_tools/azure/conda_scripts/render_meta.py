5fcf6e4d04d4e2d08a640ad0c5ec693b4a5c5cd2
Aaron Smith <aaronreidsmith@gmail.com>
2019-08-01 15:44:25 -0500

    Full path to wheel

--- build_tools/azure/conda_scripts/render_meta.py
+++ build_tools/azure/conda_scripts/render_meta.py
@@ -1,6 +1,9 @@
+import os
+
 from jinja2 import Environment, FileSystemLoader
 
 
+DIST_PATH = '../../../dist'
 REQUIREMENTS_FILE = '../../../requirements.txt'
 OUTPUT_FILE = '../../../conda/meta.yaml'  # conda is weird about yml vs yaml, so we have to use yaml
 TEMPLATE_PATH = '.'
@@ -14,10 +17,12 @@ with open('../../../requirements.txt') as file:
     requirements = [line.strip() for line in file.readlines()]
 
 numpy_version = next(package for package in requirements if 'numpy' in package)
+wheel = next(file for file in os.listdir(DIST_PATH) if file.endswith('.whl'))
 
 context = {
     'requirements': requirements,
-    'numpy_version': numpy_version
+    'numpy_version': numpy_version,
+    'wheel': wheel
 }
 
 with open(OUTPUT_FILE, 'w') as out:
