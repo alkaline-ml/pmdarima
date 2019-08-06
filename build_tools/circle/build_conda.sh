7d821438a0ecf9264d680abc9cc4225aa0a3137a
Aaron Smith <aaronreidsmith@gmail.com>
2019-08-05 19:35:06 -0500

    Build the wheel in pure python

--- build_tools/circle/build_conda.sh
+++ build_tools/circle/build_conda.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-pip install conda jinja2
+# Build the wheel first
+pip install -r requirements.txt
+python setup.py bdist_wheel
+
+# Render the meta file
+pip install jinja2
 mkdir conda
-ls
 cd build_tools/common
 python render_meta.py
 cd -
+
+# Build the conda distribution
 conda install conda-build
 conda-build --python=$1 conda/
