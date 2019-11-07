cacd7e37d2f04b74c95dbc8232d0824e1dd53ab1
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-07 17:10:49 -0600

    Updated to handle tag without v prefix

--- build_tools/get_tag.py
+++ build_tools/get_tag.py
@@ -9,8 +9,9 @@ OUT_FILE = Path(__file__).parent / 'VERSION'
 # Circle is easy, since they give us the git tag
 if os.getenv('CIRCLECI', False) and os.getenv('GIT_TAG', False):
     print('Tagged commit on Circle CI. Writing to {0}'.format(OUT_FILE))
+    tag = os.getenv('GIT_TAG')
     with open(OUT_FILE, 'w') as file:
-        file.write(os.getenv('GIT_TAG')[1:])
+        file.write(tag[1:] if tag.startswith('v') else tag)
 
 # on Azure Pipelines, we have to look at the build branch, and apply this logic:
 # refs/tags/vX.X.X -> ['refs', 'tags', 'vX.X.X'] -> 'vX.X.X'
@@ -18,7 +19,7 @@ elif os.getenv('BUILD_SOURCEBRANCH', False) and os.getenv('BUILD_SOURCEBRANCH').
     print('Tagged commit on Azure Pipelines. Writing to {0}'.format(OUT_FILE))
     tag = os.getenv('BUILD_SOURCEBRANCH').split('/')[-1]
     with open(OUT_FILE, 'w') as file:
-        file.write(tag[1:])
+        file.write(tag[1:] if tag.startswith('v') else tag)
 
 # Local or non-tagged commit, so we don't generate a VERSION file
 else:
