61aed00278eefe825399bab545b3cd3a89a130cb
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-07 16:00:19 -0600

    Fix typo

--- pmdarima/__init__.py
+++ pmdarima/__init__.py
@@ -24,9 +24,9 @@ import os as _os
 # For local development, or non-tagged commits, we will use 0.0.0
 try:
     version_path = Path(__file__).parent / "VERSION"
-    version = version_path.read_text().strip()
+    __version__ = version_path.read_text().strip()
 except FileNotFoundError:
-    version = "0.0.0"
+    __version__ = "0.0.0"
 
 try:
     # this var is injected in the setup build to enable
