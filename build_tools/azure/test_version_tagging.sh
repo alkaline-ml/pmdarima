a001801649895036fb1f1f2ffefffbccbae00c12
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-08 08:16:33 -0600

    Remove extra directory

--- build_tools/azure/test_version_tagging.sh
+++ build_tools/azure/test_version_tagging.sh
@@ -3,9 +3,9 @@
 set -e
 pip install pathlib
 
-BUILD_SOURCEBRANCH=refs/tags/v0.99.999 python ${BUILD_SOURCESDIRECTORY}/pmdarima/build_tools/get_tag.py
+BUILD_SOURCEBRANCH=refs/tags/v0.99.999 python ${BUILD_SOURCESDIRECTORY}/build_tools/get_tag.py
 
-if [[ ! -f ${BUILD_SOURCESDIRECTORY}/pmdarima/pmdarima/VERSION ]]; then
+if [[ ! -f ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
     exit 4
 fi
