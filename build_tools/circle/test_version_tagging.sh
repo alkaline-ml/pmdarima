5d6bf7ebe97c77b3265f8a2634c866f605af540b
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-07 22:46:09 -0600

    Full path to get_tag. See https://stackoverflow.com/q/24112727

--- build_tools/circle/test_version_tagging.sh
+++ build_tools/circle/test_version_tagging.sh
@@ -3,7 +3,7 @@
 set -e
 pip install pathlib
 
-GIT_TAG=0.99.999 python ../get_tag.py
+GIT_TAG=0.99.999 python ~/pmdarima/build_tools/get_tag.py
 
 if [[ ! -f ~/pmdarima/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
