d54f10d180badf6b62d25ff750c01e568016c502
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-08 08:10:14 -0600

    Uses sources directory

--- build_tools/azure/test_version_tagging.sh
+++ build_tools/azure/test_version_tagging.sh
@@ -3,9 +3,9 @@
 set -e
 pip install pathlib
 
-BUILD_SOURCEBRANCH=refs/tags/0.99.999 python ${AGENT_BUILDDIRECTORY}/pmdarima/build_tools/get_tag.py
+BUILD_SOURCEBRANCH=refs/tags/0.99.999 python ${AGENT_SOURCESDIRECTORY}/pmdarima/build_tools/get_tag.py
 
-if [[ ! -f ${AGENT_BUILDDIRECTORY}/pmdarima/pmdarima/VERSION ]]; then
+if [[ ! -f ${AGENT_SOURCESDIRECTORY}/pmdarima/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
     exit 4
 fi
