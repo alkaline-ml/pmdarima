b5383886a729d3cb10d81c899f7bd2a5c03c85bc
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-08 07:58:15 -0600

    Use env var instead of tilde

--- build_tools/circle/test_version_tagging.sh
+++ build_tools/circle/test_version_tagging.sh
@@ -3,9 +3,9 @@
 set -e
 pip install pathlib
 
-GIT_TAG=0.99.999 python ~/pmdarima/build_tools/get_tag.py
+GIT_TAG=0.99.999 python ${AGENT_BUILDDIRECTORY}/pmdarima/build_tools/get_tag.py
 
-if [[ ! -f ~/pmdarima/pmdarima/VERSION ]]; then
+if [[ ! -f ${AGENT_BUILDDIRECTORY}/pmdarima/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
     exit 4
 fi
