9b79097d2104eb8988d6f3327014a435ccbd4ac2
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-08 08:14:24 -0600

    Add 'v' to tag

--- build_tools/azure/test_version_tagging.sh
+++ build_tools/azure/test_version_tagging.sh
@@ -3,7 +3,7 @@
 set -e
 pip install pathlib
 
-BUILD_SOURCEBRANCH=refs/tags/0.99.999 python ${BUILD_SOURCESDIRECTORY}/pmdarima/build_tools/get_tag.py
+BUILD_SOURCEBRANCH=refs/tags/v0.99.999 python ${BUILD_SOURCESDIRECTORY}/pmdarima/build_tools/get_tag.py
 
 if [[ ! -f ${BUILD_SOURCESDIRECTORY}/pmdarima/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
