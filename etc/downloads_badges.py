5911add75969e4dd8f4db91afefef05efca0e350
Aaron Smith <aaronreidsmith@gmail.com>
2019-11-17 18:28:03 -0600

    Combined total and weekly downloads into one script meant to be run in github actions

--- etc/downloads_badges.py
+++ etc/downloads_badges.py
@@ -1,8 +1,4 @@
-"""
-This code is meant to be run on Zapier
-
-It is used to add the `total downloads` badge to our README
-"""
+from datetime import date, timedelta
 import json
 import math
 import os
@@ -10,7 +6,7 @@ import requests
 
 
 def millify(n):
-    """Abreviate a number to nearest thousand, million, etc.
+    """Abbreviate a number to nearest thousand, million, etc.
 
     Adapted from: https://stackoverflow.com/a/3155023/10696164
 
@@ -46,20 +42,41 @@ def millify(n):
     return f'{final_output}{millnames[millidx]}'
 
 
-# Open a session to save time (only allowed 1 second on Zapier)
+# Used to calculate downloads for the last week
+today = date.today()
+last_week = today - timedelta(days=7)
+DATE_FORMAT = '%Y-%m-%d'
+
+# Open a session to save time
 session = requests.Session()
 
 # Get the data for both the legacy namespace and our current one
 pyramid_arima = json.loads(session.get('https://api.pepy.tech/api/projects/pyramid-arima').text)
 pmdarima = json.loads(session.get('https://api.pepy.tech/api/projects/pmdarima').text)
 
-# Millify the total and save in a dict
-total = millify(pyramid_arima['total_downloads'] + pmdarima['total_downloads'])
+# Sum up pmdarima and pyramid-arima downloads to the past week
+pmdarima_downloads = 0
+for i in range(7):
+    pmdarima_downloads += pmdarima['downloads'][(last_week + timedelta(days=i)).strftime(DATE_FORMAT)]
+
+pyramid_arima_downloads = 0
+for i in range(7):
+    pyramid_arima_downloads += pyramid_arima['downloads'][(last_week + timedelta(days=i)).strftime(DATE_FORMAT)]
+
+# Millify the totals
+total_downloads = millify(pyramid_arima['total_downloads'] + pmdarima['total_downloads'])
+weekly_downloads = millify(pmdarima_downloads + pyramid_arima_downloads)
+
 data = {
-    'total': total
+    'total': total_downloads,
+    'weekly': weekly_downloads
 }
 
-# Write to our storage area
-# StoreClient is Zapier-specific and automatically imported (hence no import)
-store = StoreClient(os.environ.get('ZAPIER_SHA'))
-store.set('data', data)
+request = session.post(
+    url='https://store.zapier.com/api/records',
+    headers={
+        'X-Secret':  os.getenv('ZAPIER_SHA')
+    },
+    data=json.dumps(data)
+)
+request.raise_for_status()
