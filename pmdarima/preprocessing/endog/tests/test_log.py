17d2d4eaf58011ceb33a4d5944253578c2b5edd1
Christopher Siewert <christopher.siewert@live.ca>
2019-11-18 23:03:09 -0800

    Add test_invertible to log transformer test

--- pmdarima/preprocessing/endog/tests/test_log.py
+++ pmdarima/preprocessing/endog/tests/test_log.py
@@ -8,10 +8,19 @@ import pytest
 from pmdarima.preprocessing import LogEndogTransformer
 from pmdarima.preprocessing import BoxCoxEndogTransformer
 
+
 def test_same():
     y = [1, 2, 3]
-    trans = BoxCoxEndogTransformer()
+    trans = BoxCoxEndogTransformer(lmbda=0)
     log_trans = LogEndogTransformer()
     y_t, _ = trans.fit_transform(y)
     log_y_t, _ = log_trans.fit_transform(y)
     assert_array_almost_equal(log_y_t, y_t)
+
+
+def test_invertible():
+    y = [1, 2, 3]
+    trans = LogEndogTransformer()
+    y_t, _ = trans.fit_transform(y)
+    y_prime, _ = trans.inverse_transform(y_t)
+    assert_array_almost_equal(y, y_prime)
