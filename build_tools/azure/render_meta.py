aaa4a69ffc57a768d3ca1dd9e7833a2c6e8128ed
Aaron Smith <aaronreidsmith@gmail.com>
2019-12-11 15:18:24 -0600

    Clean up jobs

--- build_tools/azure/render_meta.py
+++ build_tools/azure/render_meta.py
@@ -3,6 +3,7 @@ import sys
 
 from jinja2 import Environment, FileSystemLoader
 
+# Constant file paths. Relative to this file, so we have to run this file from this directory
 DIST_PATH = '../../dist'
 VERSION_FILE = '../../VERSION'
 REQUIREMENTS_FILE = '../../requirements.txt'
@@ -15,20 +16,26 @@ TEMPLATE_ENVIRONMENT = Environment(
     trim_blocks=False
 )
 
+# Find the version
 try:
     VERSION = open(VERSION_FILE).readline().strip()
 except FileNotFoundError:
     VERSION = '0.0.0'
 
+# Find the requirements and versions
 with open(REQUIREMENTS_FILE) as file:
     requirements = [line.strip() for line in file.readlines()]
 
+# We build from source on windows, otherwise, we looks for a wheel
 if sys.platform != 'win32':
     wheel = next(file for file in os.listdir(DIST_PATH) if file.endswith('.whl'))
 else:
     wheel = None
+
+# Numpy version is used for building
 numpy_version = next(package for package in requirements if 'numpy' in package)
 
+# Render and write the meta.yaml file to $ROOT/conda/meta.yaml
 context = {
     'requirements': requirements,
     'numpy_version': numpy_version,
@@ -41,5 +48,5 @@ context = {
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 with open(OUTPUT_FILE, 'w') as out:
-    meta = TEMPLATE_ENVIRONMENT.get_template('meta_template.yml').render(context)
+    meta = TEMPLATE_ENVIRONMENT.get_template('meta_template.yml.j2').render(context)
     out.write(meta)
