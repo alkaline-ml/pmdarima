208a86cf6062d104c99c0ca14230ab22e52156bb
Aaron Smith <aaronreidsmith@gmail.com>
2019-12-26 10:46:14 -0600

    Add comments

--- build_tools/circle/deploy.sh
+++ build_tools/circle/deploy.sh
@@ -3,9 +3,9 @@
 set -e -x
 
 pip install twine wheel
+# If the VERSION ends with a letter (beta, rc, etc.), it is considered a test release
 if [[ $CIRCLE_TAG =~ '[a-zA-Z]'$ ]]; then
   twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
 else
   twine upload --skip-existing dist/pmdarima-*
 fi
-# twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
