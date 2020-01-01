037c978d58d9cea8b8d1d20e6508856b983b9b11
Aaron Smith <aaronreidsmith@gmail.com>
2020-01-01 13:36:22 -0600

    Add ability to test CI/CD using rc tags

--- build_tools/circle/deploy.sh
+++ build_tools/circle/deploy.sh
@@ -3,5 +3,18 @@
 set -e -x
 
 pip install twine wheel
-twine upload --skip-existing dist/pmdarima-*
-# twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
+
+# Check our VERSION. Basically, if it contains letters, it is a pre-release. Otherwise,
+# it has to match X.Y or X.Y.Z
+#
+# On CircleCI, we look for the `v` at the beginning of the version, since we are looking at the tag
+if [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?[a-zA-Z]+[0-9]*$' ]]; then
+  echo 'Uploading to test pypi'
+  twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
+elif [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?$' ]]; then
+  echo 'Uploading to production pypi'
+  twine upload --skip-existing dist/pmdarima-*
+else
+  echo 'Malformed tag'
+  exit 1
+fi
