f55bb2a447eb6ca45789c188aa391b4b1c1d5010
Aaron Smith <aaronreidsmith@gmail.com>
2020-01-01 12:52:42 -0600

    Address PR comments

--- build_tools/circle/deploy.sh
+++ build_tools/circle/deploy.sh
@@ -9,8 +9,10 @@ pip install twine wheel
 #
 # On CircleCI, we look for the `v` at the beginning of the version, since we are looking at the tag
 if [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?[a-zA-Z]+[0-9]*$' ]]; then
+  echo 'Uploading to test pypi'
   twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
 elif [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?$' ]]; then
+  echo 'Uploading to production pypi'
   twine upload --skip-existing dist/pmdarima-*
 else
   echo 'Malformed tag'
