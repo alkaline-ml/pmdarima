db4a587195b4739c1d5cd7def0ca98c6153fe1cb
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-14 07:41:49 -0600

    [MRG+1] Hotfix for 1.5.3 deploy (#295)
    * Change how we check for a tag on github
    
    * Fix our version check step
    
    * Ensure VERSION file created on windows
    
    * Add condition
    
    * Add version check test
    
    * Fix regex and move deploy to its own file
    
    * Fix github deploy step
    
    * Python 3.8 fix with red hat image
    
    * Fix circle regex as well
    
    * Change conditional order
    
    * Bash does not like quotes

---
+++ build_tools/azure/conda/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This looks like it is running `conda build` again, but it just returns the output file
+output_file=$(conda-build --output --python=$(python.version) conda/)
+
+# Check our VERSION. Basically, if it contains letters, it is a pre-release. Otherwise,
+# it has to match X.Y or X.Y.Z
+#
+# --skip means skip existing (can be a little confusing)
+if [[ $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION) =~ ^[0-9]+\.[0-9]+\.?[0-9]*?[a-zA-Z]+[0-9]*$ ]]; then
+  # Adding the `test` label means it doesn't show up unless you specifically
+  # search for packages with the label `test`
+  echo 'Uploading to conda with test label'
+  anaconda upload --label test --skip $output_file
+elif [[ $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION) =~ ^[0-9]+\.[0-9]+\.?[0-9]*$ ]]; then
+  echo 'Uploading to production conda channel'
+  anaconda upload --skip $output_file
+else
+  echo "Malformed tag: $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION)"
+  exit 1
+fi
---
+++ build_tools/github/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $(cat ${GITHUB_WORKSPACE}/pmdarima/VERSION) =~ ^[0-9]+\.[0-9]+\.?[0-9]*[a-zA-Z]+[0-9]*$ ]]; then
+  echo 'Uploading to test pypi'
+  python -m twine upload --repository-url https://test.pypi.org/legacy/ --skip-existing dist/pmdarima-*
+elif [[ $(cat ${GITHUB_WORKSPACE}/pmdarima/VERSION) =~ ^[0-9]+\.[0-9]+\.?[0-9]*$ ]]; then
+  echo 'Uploading to production pypi'
+  python -m twine upload --skip-existing dist/pmdarima-*
+else
+  echo "Malformed tag": $(cat ${GITHUB_WORKSPACE}/pmdarima/VERSION)""
+  exit 1
+fi
