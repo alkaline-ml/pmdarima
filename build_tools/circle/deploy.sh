f611d81e64c5d9914bbf9a6b905814dd8c2120d9
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-14 07:09:16 -0600

    Fix circle regex as well

--- build_tools/circle/deploy.sh
+++ build_tools/circle/deploy.sh
@@ -8,10 +8,10 @@ pip install twine wheel
 # it has to match X.Y or X.Y.Z
 #
 # On CircleCI, we look for the `v` at the beginning of the version, since we are looking at the tag
-if [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?[a-zA-Z]+[0-9]*$' ]]; then
+if [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*[a-zA-Z]+[0-9]*$' ]]; then
   echo 'Uploading to test pypi'
   twine upload --skip-existing --repository-url https://test.pypi.org/legacy/ dist/pmdarima-*
-elif [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*?$' ]]; then
+elif [[ ${CIRCLE_TAG} =~ '^v?[0-9]+\.[0-9]+\.?[0-9]*$' ]]; then
   echo 'Uploading to production pypi'
   twine upload --skip-existing dist/pmdarima-*
 else
