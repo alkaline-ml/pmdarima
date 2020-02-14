7505f91accb75d48d2c05ac5eafdd589d7bbbb4d
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-13 18:02:26 -0600

    Add version check test

--- build_tools/azure/test_version_tagging.sh
+++ build_tools/azure/test_version_tagging.sh
@@ -5,6 +5,19 @@ pip install pathlib
 
 BUILD_SOURCEBRANCH=refs/tags/v0.99.999 python ${BUILD_SOURCESDIRECTORY}/build_tools/get_tag.py
 
+if [[ $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION) =~ '^[0-9]+\.[0-9]+\.?[0-9]*?[a-zA-Z]+[0-9]*$' ]]; then
+  # Adding the `test` label means it doesn't show up unless you specifically
+  # search for packages with the label `test`
+  echo 'Uploading to conda with test label'
+  anaconda upload --label test --skip $output_file
+elif [[ $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION) =~ '^[0-9]+\.[0-9]+\.?[0-9]*?$' ]]; then
+  echo 'Uploading to production conda channel'
+  anaconda upload --skip $output_file
+else
+  echo "Malformed tag: $(cat ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION)"
+  exit 1
+fi
+
 if [[ ! -f ${BUILD_SOURCESDIRECTORY}/pmdarima/VERSION ]]; then
     echo "Expected VERSION file"
     exit 4
