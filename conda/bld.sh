6be43642f2380ceb27d8fda8fd1199591f44ddd5
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-17 10:02:46 -0600

    Added echo statements

--- conda/bld.sh
+++ conda/bld.sh
@@ -2,15 +2,23 @@
 
 # This is run from the root directory, so we treat it as such
 
+echo "In bld.sh"
+
 # Build our distribution first
 python -m pip install --no-deps --ignore-installed .
+
+echo "Installed pmdarima"
 
 # Find where the package was installed
 site_packages=$(python -c 'import site; print(site.getsitepackages()[0])')
 
+echo "Found site_packages: ${site_packages}"
+
 # Make the output directory
 output_dir=${PREFIX}/lib/python${PY_VER}/site-packages/
 mkdir $output_dir
+
+echo "Made output_dir: ${output_dir}"
 
 # Copy the build artifacts to the conda output directory
 cp ${site_packages}/${PKG_NAME} $output_dir
