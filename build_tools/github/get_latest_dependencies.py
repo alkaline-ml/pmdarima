c468d450f28e59f68694c04ed4e617b6ea03139d
Aaron Smith <aaronreidsmith@gmail.com>
2020-02-23 18:07:33 -0600

    Install build tools too

--- build_tools/github/get_latest_dependencies.py
+++ build_tools/github/get_latest_dependencies.py
@@ -6,12 +6,25 @@ from os.path import abspath, dirname
 
 TOP_LEVEL = abspath(dirname(dirname(dirname(__file__))))
 REQUIREMENTS = os.path.join(TOP_LEVEL, 'requirements.txt')
+BUILD_REQUIREMENTS = os.path.join(TOP_LEVEL, 'build_tools', 'build_requirements.txt')
 
-requirements = []
+requirements = []  # We use a list instead of a set because we want to maintain order
 with open(REQUIREMENTS) as file:
     for line in file:
         requirement = line.strip()
+        if line.startswith('#'):
+            continue
         match = re.match(r'^([A-Za-z\-0-9]+)', requirement)
-        requirements.append(match.group(0))
+        if match.group(0).lower() not in requirements:
+            requirements.append(match.group(0).lower())
 
-print(' '.join(requirements))
+with open(BUILD_REQUIREMENTS) as file:
+    for line in file:
+        requirement = line.strip()
+        if line.startswith('#'):
+            continue
+        match = re.match(r'^([A-Za-z\-0-9]+)', requirement)
+        if match.group(0).lower() not in requirements:
+            requirements.append(match.group(0).lower())
+
+print(' '.join(list(requirements)))
