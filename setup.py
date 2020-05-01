2e4639579e47f20616f505e35e1c6c1dcd9a827c
Aaron Smith <aaronreidsmith@gmail.com>
2020-05-01 08:13:10 -0500

    Add 3.8 tag to pypi

--- setup.py
+++ setup.py
@@ -234,6 +234,7 @@ def do_setup():
                         'Programming Language :: Python :: 3.5',
                         'Programming Language :: Python :: 3.6',
                         'Programming Language :: Python :: 3.7',
+                        'Programming Language :: Python :: 3.8',
                         ('Programming Language :: Python :: '
                          'Implementation :: CPython'),
                         ('Programming Language :: Python :: '
