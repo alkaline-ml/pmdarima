56e346e524aad1e7404a751b3d9173033c532022
tgsmith61591 <tgsmith61591@gmail.com>
2020-06-29 09:35:58 -0500

    Add test of varargs

--- pmdarima/tests/test_context_managers.py
+++ pmdarima/tests/test_context_managers.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from pmdarima import context_managers as ctx
+from pmdarima.compat.pytest import pytest_error_str
+
+import pytest
 
 
 def test_except_and_reraise_do_reraise():
     try:
         with ctx.except_and_reraise(
-                ValueError, "foo message", KeyError, "bar message"
+                ValueError,
+                raise_err=KeyError,
+                raise_msg="bar message"
         ):
             raise ValueError("contains foo message")
     except KeyError as k:
@@ -19,10 +24,29 @@ def test_except_and_reraise_do_reraise():
 def test_except_and_reraise_no_reraise():
     try:
         with ctx.except_and_reraise(
-                ValueError, "foo message", KeyError, "bar message"
+                ValueError,
+                raise_err=TypeError,
+                raise_msg="bar message"
         ):
-            raise ValueError("some other error")
-    except ValueError as v:
-        assert "some other error" in str(v)
+            raise KeyError("foo message")
+    except KeyError as v:
+        assert "foo message" in str(v)
     else:
         assert False, "Test failed"
+
+
+@pytest.mark.parametrize('err', [ValueError, KeyError, TypeError])
+def test_multiple(err):
+
+    class FooError(BaseException):
+        pass
+
+    with pytest.raises(FooError) as fe:
+        with ctx.except_and_reraise(
+                ValueError, KeyError, TypeError,
+                raise_err=FooError,
+                raise_msg="gotcha, fam",
+        ):
+            raise err("Boo!")
+
+    assert "gotcha, fam" in pytest_error_str(fe)
