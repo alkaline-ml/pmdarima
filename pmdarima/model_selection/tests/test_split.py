c91e1851eac0bf5e7d2a28b7a0d63a1df8641873
tgsmith61591 <tgsmith61591@gmail.com>
2020-07-28 15:15:25 -0500

    Make test more complete, add in whats_new

--- pmdarima/model_selection/tests/test_split.py
+++ pmdarima/model_selection/tests/test_split.py
@@ -101,5 +101,21 @@ def test_issue_364_bad_splits():
     cv = SlidingWindowForecastCV(window_size=90, step=1, h=4)
     gen = cv.split(endog)
 
+    expected = [
+        (np.arange(0, 90), np.array([90, 91, 92, 93])),
+        (np.arange(1, 91), np.array([91, 92, 93, 94])),
+        (np.arange(2, 92), np.array([92, 93, 94, 95])),
+        (np.arange(3, 93), np.array([93, 94, 95, 96])),
+        (np.arange(4, 94), np.array([94, 95, 96, 97])),
+        (np.arange(5, 95), np.array([95, 96, 97, 98])),
+        (np.arange(6, 96), np.array([96, 97, 98, 99])),
+    ]
+
     # should be 7
-    assert len(list(gen)) == 7
+    for i, (train, test) in enumerate(gen):
+        assert_array_equal(train, expected[i][0])
+        assert_array_equal(test, expected[i][1])
+
+    # assert no extra splits
+    with pytest.raises(StopIteration):
+        next(gen)
