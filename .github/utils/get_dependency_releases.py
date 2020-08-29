43f1ab774f7637f54514436658bf2e6077b8f20f
Aaron Smith <aaronreidsmith@gmail.com>
2020-08-29 11:21:12 -0500

    See if blocks work

--- .github/utils/get_dependency_releases.py
+++ .github/utils/get_dependency_releases.py
@@ -11,28 +11,31 @@ session = requests.Session()
 
 # This is the format for a two-column table. We start with the headers, and
 # build up the rest in the below loop. https://stackoverflow.com/a/55734396/10696164
-releases = [
-    {
-        "type": "mrkdwn",
-        "text": "*Package*"
-    },
-    {
-        "type": "mrkdwn",
-        "text": "*Release Date*"
-    }
-]
+releases = {
+    'type': 'section',
+    'fields': [
+        {
+            "type": "mrkdwn",
+            "text": "*Package*"
+        },
+        {
+            "type": "mrkdwn",
+            "text": "*Release Date*"
+        }
+    ]
+}
 for package in packages:
     pypi = session.get(f'https://pypi.org/pypi/{package}/json').json()
     latest_version = pypi['info']['version']
     latest_release_date = pypi['releases'][latest_version][0]['upload_time']
-    releases.append({
+    releases['fields'].append({
         'type': 'plain_text',
         'text': f'{package} {latest_version}'
     })
-    releases.append({
+    releases['fields'].append({
         'type': 'plain_text',
         'text': latest_release_date.replace('T', ' ') + ' UTC'
     })
 
 session.close()
-print(releases) # this will be used in GitHub Actions
+print([releases])  # this will be used in GitHub Actions
