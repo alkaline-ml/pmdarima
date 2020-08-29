c216128151940b6c7ca480d88454f6d07b2991a4
Aaron Smith <aaronreidsmith@gmail.com>
2020-08-29 10:50:09 -0500

    Add logic to post to slack

--- .github/utils/get_dependency_releases.py
+++ .github/utils/get_dependency_releases.py
@@ -9,13 +9,30 @@ if len(sys.argv) < 2:
 packages = sys.argv[1:]
 session = requests.Session()
 
-# Build up our table of release dates
-releases = {}
+# This is the format for a two-column table. We start with the headers, and
+# build up the rest in the below loop. https://stackoverflow.com/a/55734396/10696164
+releases = [
+    {
+        "type": "mrkdwn",
+        "text": "*Package*"
+    },
+    {
+        "type": "mrkdwn",
+        "text": "*Release Date*"
+    }
+]
 for package in packages:
     pypi = session.get(f'https://pypi.org/pypi/{package}/json').json()
     latest_version = pypi['info']['version']
     latest_release_date = pypi['releases'][latest_version][0]['upload_time']
-    releases[package] = latest_release_date.replace('T', ' ') + ' UTC'
+    releases.append({
+        'type': 'plain_text',
+        'text': f'{package} {latest_version}'
+    })
+    releases.append({
+        'type': 'plain_text',
+        'text': latest_release_date.replace('T', ' ') + ' UTC'
+    })
 
 session.close()
-# Format as a slack message to return to GitHub Action
+print(releases) # this will be used in GitHub Actions
