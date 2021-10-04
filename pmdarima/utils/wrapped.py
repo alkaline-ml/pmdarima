f7073916ee8d88d93f86abbd3b2ee1fb6c678f4a
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-04 08:25:13 -0500

    Add compatibility for statsmodels 0.13 and higher

--- pmdarima/utils/wrapped.py
+++ pmdarima/utils/wrapped.py
@@ -3,10 +3,12 @@
 # Taylor G Smith <taylor.smith@alkaline.ml>
 #
 # Wrapped functions
+from functools import wraps
+from pkg_resources import parse_version
+import warnings
 
 from statsmodels.tsa.stattools import acf as sm_acf, pacf as sm_pacf
-
-from functools import wraps
+import statsmodels
 
 __all__ = [
     'acf',
@@ -54,13 +56,46 @@ def inheritdoc(parent):
 
 
 @inheritdoc(parent=sm_acf)
-def acf(x, unbiased=False, nlags=40, qstat=False, fft=False,
-        alpha=None, missing='none'):
-    return sm_acf(x=x, unbiased=unbiased, nlags=nlags,
-                  qstat=qstat, fft=fft, alpha=alpha,
-                  missing=missing)
+def acf(x, unbiased=None, nlags=40, qstat=False, fft=False,
+        alpha=None, missing='none', adjusted=False):
+    kwargs = {
+        "x": x,
+        "nlags": nlags,
+        "qstat": qstat,
+        "fft": fft,
+        "alpha": alpha,
+        "missing": missing,
+    }
+
+    # Handle kwarg deprecation in statsmodels 0.13.0
+    if parse_version(statsmodels.__version__) >= parse_version("0.13.0"):
+        if unbiased is not None:
+            warnings.warn(
+                "The `unbiased` key-word has been deprecated in "
+                "statsmodels >= 0.13.0. Please use `adjusted` instead.",
+                DeprecationWarning
+            )
+            kwargs["adjusted"] = unbiased
+        else:
+            kwargs["adjusted"] = adjusted
+    else:
+        if unbiased is not None:
+            kwargs["unbiased"] = unbiased
+        else:
+            kwargs["unbiased"] = adjusted
+
+    return sm_acf(**kwargs)
 
 
 @inheritdoc(parent=sm_pacf)
 def pacf(x, nlags=40, method='ywunbiased', alpha=None):
+    # Handle kwarg deprecation in statsmodels 0.13.0
+    if parse_version(statsmodels.__version__) >= parse_version("0.13.0") and "unbiased" in method:
+        warnings.warn(
+            "The `*unbiased` methods have been deprecated in "
+            "statsmodels >= 0.13.0. Please use `*adjusted` instead.",
+            DeprecationWarning
+        )
+        method = method.replace("unbiased", "adjusted")
+
     return sm_pacf(x=x, nlags=nlags, method=method, alpha=alpha)
