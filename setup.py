21153777160f3bc70cebfd451bb6fb99b7f6f542
Aaron Smith <aaronreidsmith@gmail.com>
2021-10-07 08:21:16 -0500

    Add builds for 3.10

--- setup.py
+++ setup.py
@@ -242,6 +242,7 @@ def do_setup():
                         'Programming Language :: Python :: 3.7',
                         'Programming Language :: Python :: 3.8',
                         'Programming Language :: Python :: 3.9',
+                        'Programming Language :: Python :: 3.10',
                         ('Programming Language :: Python :: '
                          'Implementation :: CPython'),
                     ],
