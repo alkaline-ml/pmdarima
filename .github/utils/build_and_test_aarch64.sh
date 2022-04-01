8c1bac0a9590848bdb0b21f2f842ef65a2828ec0
Aaron Smith <aaronreidsmith@gmail.com>
2022-04-01 15:01:45 -0500

    Add auditwheel

--- .github/utils/build_and_test_aarch64.sh
+++ .github/utils/build_and_test_aarch64.sh
@@ -15,6 +15,16 @@ pip install -r requirements.txt
 # Make our wheel
 make version bdist_wheel
 
+# Audit our wheel
+pip install auditwheel
+for whl in dist/*.whl; do
+    if [[ "$whl" =~ "pmdarima" ]]; then
+        auditwheel repair $whl -w dist/ #repair pmdarima wheel and output to dist/
+    fi
+    rm "$whl" # remove original wheel
+done
+
 # Testing on aarch64 takes too long, so we simply import the package as a spot test
 pip install --pre --no-index --find-links dist/ pmdarima
+cd
 $PYTHON -c 'import pmdarima; pmdarima.show_versions()'
