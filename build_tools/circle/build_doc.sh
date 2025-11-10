1e41976fc8b586efaa3cee03a7d3f49fd5cb8f97
tgsmith61591 <tgsmith61591@gmail.com>
2025-11-10 15:15:27 -0600

    Voodoo

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 make install
-pip install pandas sphinx sphinx_gallery pytest-runner sphinx_rtd_theme "matplotlib>=2.2.0" numpydoc image
+pip install pandas sphinx sphinx_gallery pytest-runner sphinx_rtd_theme "matplotlib>=2.2.0" image
+pip install --upgrade numpydoc
 
 # this is a hack, but we have to make sure we're only ever running this from
 # the top level of the package and not in the subdirectory...
